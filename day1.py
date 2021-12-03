86cf42202b99f735aedd20592c4ecb0d6c926d7f
Gonzalo Rubio <gonzalo@bellmetric.net>
2021-12-03 18:43:44 +0100

    Horrible solution for day1-1

---
+++ day1.py
@@ -0,0 +1,10 @@
+from aocd import data
+
+input_list = [int(n) for n in data.splitlines()]
+increase = 0
+i = 1
+while i < len(input_list):
+    if input_list[i-1] < input_list[i]:
+        increase = increase + 1
+    i = i+1
+print(increase)
